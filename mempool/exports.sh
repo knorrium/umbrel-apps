b35d34faa36df526b7846b0f850d99fb7bb68708
Felipe Knorr Kuhn <100320+knorrium@users.noreply.github.com>
2023-01-25 21:37:36 -0800

    Test app settings with dual backend setup

--- mempool/exports.sh
+++ mempool/exports.sh
@@ -3,5 +3,9 @@ export APP_MEMPOOL_PORT="3006"
 export APP_MEMPOOL_API_IP="10.21.21.27"
 export APP_MEMPOOL_DB_IP="10.21.21.28"
 
+export APP_MEMPOOL_LIGHTNING_API_IP="10.21.21.29"
+export APP_MEMPOOL_LIGHTNING_DB_IP="10.21.21.30"
+
+
 hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}/hostname"
 export APP_MEMPOOL_HIDDEN_SERVICE="$(cat "${hidden_service_file}" 2>/dev/null || echo "")"
